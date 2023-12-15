b4e4baa7ad65975c162b70d7f6998e3b29f3b302
Jessica Keller <jessica.keller68@gmail.com>
2023-12-15 10:09:49 +0100

    point 4

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/boat')]
 class BoatController extends AbstractController
 {
+
     #[Route('/move/{x<\d+>}/{y<\d+>}', name: 'moveBoat')]
     public function moveBoat(
         int $x,
@@ -22,6 +23,41 @@ class BoatController extends AbstractController
         
         $boat->setCoordX($x);
         $boat->setCoordY($y);
+
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('map');
+    }
+
+    #[Route('/direction/{direction}', methods: ['GET'], requirements: ["direction" => "[N]|[S]|[W]|[E]"], name:'direction')]
+    public function moveDirection(
+        string $direction,
+        BoatRepository $boatRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+
+        /** Recherche les coordonnées actuelles du bateau */
+        $boat = $boatRepository->findOneBy([]);
+        
+        if (!$boat) {
+            throw $this->createNotFoundException('No boat found');
+        }
+
+        /** Mise à jour de la position du bateau en se basant sur la direction */
+        switch ($direction) {
+            case 'N':
+                $boat->setCoordY($boat->getCoordY() - 1);
+                break;
+            case 'S':
+                $boat->setCoordY($boat->getCoordY() + 1);
+                break;
+            case 'E':
+                $boat->setCoordX($boat->getCoordX() + 1);
+                break;
+            case 'W':
+                $boat->setCoordX($boat->getCoordX() - 1);
+                break;
+        }
 
         $entityManager->flush();
         
