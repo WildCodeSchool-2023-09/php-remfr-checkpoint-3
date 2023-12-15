19f05dbb0363f95cf49badb97f25aa08c9eaec97
Dev-Djam <145383391+Dev-Djam@users.noreply.github.com>
2023-12-15 12:40:55 +0100

    fix

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -27,4 +27,32 @@ class BoatController extends AbstractController
         
         return $this->redirectToRoute('map');
     }
+      public function moveDirection(string $direction,
+
+        BoatRepository $boatRepository,
+        EntityManagerInterface $entityManager
+        ): Response {
+                $boat = $boatRepository->findOneBy([]);
+            if ($direction ==="N" ){
+                $boat->setCoordY($boat->getCoordY() -1);
+            }
+            if ($direction === "S"){
+                $boat->setCoordY($boat->getCoordY() +1);
+            }
+            if ($direction === "E"){
+                $boat->setCoordX($boat->getCoordX() +1);
+            }
+            if ($direction === "O"){
+                $boat->setCoordX($boat->getCoordX() -1);
+                if (!in_array($direction, ['N', 'S', 'E', 'W'])) {
+
+                    throw $this->createNotFoundException('Error 404');
+
+
+}
+       return $this->redirectToRoute("map");
+
+        }
+
+}
 }
