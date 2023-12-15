8773714e888c7a7a2df9efdcb4a0a51263316807
Laura WOLFF <laura.wolff@live.fr>
2023-12-15 10:18:58 +0100

    implemented the moveDirection function in BoatController

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -7,10 +7,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Boat;
 
 #[Route('/boat')]
 class BoatController extends AbstractController
 {
+    protected array $cardinal = ['N', 'S', 'E', 'W'];
     #[Route('/move/{x<\d+>}/{y<\d+>}', name: 'moveBoat')]
     public function moveBoat(
         int $x,
@@ -25,6 +27,33 @@ class BoatController extends AbstractController
 
         $entityManager->flush();
         
+        return $this->redirectToRoute('map');
+    }
+
+    #[Route('/direction/{direction}', name: 'moveDirection', methods: ['GET'])]
+    public function moveDirection(string $direction, BoatRepository $boatRepository,
+    EntityManagerInterface $entityManager): Response 
+    {
+        $boat = $boatRepository->findOneBy([]);
+        $boatY = $boat->getCoordY();
+        $boatX = $boat->getCoordX();
+
+        if(in_array($direction, $this->cardinal)){
+            if($direction === 'N'){
+                $boat->setCoordY($boatY -= 1);
+            }
+            if($direction === 'S'){
+                $boat->setCoordY($boatY += 1);
+            }
+            if($direction === 'E'){
+                $boat->setCoordX($boatX += 1);
+            }
+            if($direction === 'W'){
+                $boat->setCoordX($boatX -= 1);
+            }
+        }
+        $entityManager->flush();
+
         return $this->redirectToRoute('map');
     }
 }
